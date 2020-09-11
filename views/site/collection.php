d3f0ef651687650306d3e9841cfb47fa19712cbc
Танана Арина Сергеевна <uit32@maz.local>
2020-09-11 14:28:32 +0300

    Collection: GridView

--- views/site/collection.php
+++ views/site/collection.php
@@ -8,38 +8,28 @@ use app\models\Collection;
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\GridView;
+use yii\data\ActiveDataProvider;
 
 $this->title = 'Коллекция';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="content">
-    <?php
-    if (count($model)) {
-        foreach ($model as $item) {
-            $tags = $item->tags;
-            echo "<div class='list-group'>
-                    <div class='list-group-item list-group-item-action'>
-                        <div class='d-flex w-100 justify-content-between'>
-                            <h5 class='mb-1'> {$item->title} </h5>
-                            <small>3 days ago</small>
-                        </div>
-                    <div class='d-flex justify-content-start'>";
-            foreach ($tags as $tag) {
-                echo "<div class='d-inline-block mr-3'>";
-                ?>
-                <?= Html::a($tag->title, Url::to(['site/search', 'id' => $tag->id]), ['data-method' => 'POST', 'class' => 'btn btn-outline-success btn-sm']); ?>
-                <?php
-                echo "</div>";
-            }
-            echo "</div>
-                    <p class='mb-1'>Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-                    <small>Donec id elit non mi porta.</small>
-                    </div>
-                </div>";
-        }
-    } else {
-        echo "<div class='alert alert-danger'>Коллекция пустая</div>";
-    }
-    ?>
-
+    <?php if ($model->owner_id == Yii::$app->user->id): ?>
+        <?= Html::button('Добавить айтем', ['class' => 'btn btn-primary ',
+            'name' => 'add-item-button',
+            'data-toggle' => 'modal',
+            'data-target' => '#add-item-modal']) ?>
+    <?php endif; ?>
+    <?= GridView::widget([
+        'dataProvider' => new ActiveDataProvider([
+            'query' => Collection::findOne($model->id)->getitems(),
+            'pagination' => [
+                'pageSize' => 20,
+            ],
+        ]),
+        'columns' => [
+            'title',
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
 </div>
